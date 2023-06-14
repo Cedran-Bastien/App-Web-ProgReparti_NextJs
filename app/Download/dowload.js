684ecf331ff5597fee886685111f243458d070e0
cedran <cedran.bastien1@etu.univ-lorraine.fr>
2023-06-14 13:34:31 +0200

    Upgrade download view

--- app/Download/dowload.js
+++ app/Download/dowload.js
@@ -1,17 +1,62 @@
 import React from 'react';
+import {Card, CardAlert} from "@/app/utils";
+
+
+// Parameters
+const Parameters = ({parameters}) => {
+    let i = 1
+    const html = parameters.map((item) => {
+        return(
+            <div>
+                <p className={"pl-5"}>{i}. {item.name} : {item.explain}</p>
+            </div>
+        )
+        i= i+1
+    })
+
+    return (
+        <div>
+            <p className={"underline"}>Parameters : </p>
+            {html}
+        </div>
+    )
+}
+
+
 
 
 // Button to dowload what we have
-const DownloadHandler  = ({title,filePath, buttonName}) => (
-    <div className={"flex flex-col content-between gap-2"}>
-        <h1>{title}</h1>
-        <a className={"ml-8 w-max p-2.5 rounded hover:bg-cyan-500 duration-500 bg-cyan-900 "} href={filePath} target={"_blank"}> Download {buttonName} </a>
-    </div>
-
+const DownloadHandler  = ({ filePath, buttonName}) => (
+        (<div className={"flex flex-col content-between mb-6 gap-2"}>
+            <a className={"ml-8 w-max p-2.5 rounded hover:bg-cyan-400 duration-500 bg-cyan-600 "} href={filePath} target={"_blank"}> Download {buttonName} </a>
+        </div>)
 )
 
-export const Download = () => (
-    <div className={"p-5 mb-8"}>
-        <DownloadHandler title={"- Client demandant les calcules"} filePath='test.zip' buttonName={"Client"} />
-    </div>
-)
+export const Download = () => {
+    const parCentral = [
+        {name: "port", explain: "port where to export the service"},
+    ]
+    const parCalcule = [
+        {name: "IP", explain: "IP of the central service"},
+    ]
+    const parClient = [
+        {name: "IP", explain: "IP of the central service"},
+    ]
+    return (
+        <div className={"p-5 mb-8 "}>
+            <Card title={"Usage"} titleColor={"text-red-600"} content={"For all State off the architecture, Unzip the compress archive and run the Start.java file"} />
+            <Card title={"Client defendant les calcules"} titleColor={"text-blue-800"} content={<Parameters parameters={parClient} />} HtmlContent={
+                <DownloadHandler filePath='test.zip' buttonName={"Client"}/>
+            }
+            />
+            <Card title={"Service central"} titleColor={"text-blue-800"} content={<Parameters parameters={parCentral} />} HtmlContent={
+                <DownloadHandler filePath={"test.zip"} buttonName={"Server"}/>
+            }
+            />
+            <Card title={"Service calculate"} titleColor={"text-blue-800"} content={<Parameters parameters={parCalcule} />} HtmlContent={
+                <DownloadHandler filePath={"test.zip"} buttonName={"Calculator"}/>
+            }
+            />
+        </div>
+    )
+}
--- app/Home/home.js
+++ app/Home/home.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import {Card} from "@/app/utils";
 import Image from "next/image";
 
-const Card = ({title, content, imagePath, width}) => {
-    const image = imagePath != null ? <Image width={width} height="0" src={imagePath} alt={imagePath}/> : ""
-    console.log(image)
-    return (
 
-        <div className="m-10 p-5 flex flex-col content-around border-solid border-2 border-black rounded-lg gap-4">
-            <h1 className={"text-2xl text-blue-800 font-bold"}>{title} :</h1>
-            <p>{content}</p>
-            {image}
-        </div>
+
+export const Home = () => {
+    const image =  (<Image width={300} height={0} src={"/logo.png"} alt={"/logo.png"}/>)
+    return (
+            <Card title="Première approche du problème" titleColor={"text-blue-800"} width={"300"} content="c'est bon la" HtmlContent={image}/>
     )
 }
-
-export const Home = () => (
-    <Card title="Première approche du problème" width={"300"} content="c'est bon la" imagePath="/logo.png"/>
-)
