4242651d48f9a56efc6a0875240889ef4c56cb71
Cedran Bastien <cedran.bastien1@etu.univ-lorraine.fr>
2023-06-15 21:02:39 +0200

    Test fix deployment problem

--- app/page.js
+++ app/page.js
@@ -1,14 +1,7 @@
 'use client'
 
 import {useRouter} from "next/navigation";
-import {REACT_LOADABLE_MANIFEST} from "next/constants";
-
-const Redirect = () => {
-    useRouter().push('/Home')
-}
 
 export default function redirect() {
-    return (
-        <Redirect/>
-    )
+    useRouter().push('/Home')
 }
