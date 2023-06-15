833d7aa58aee626b9da811d2481a858032d21c14
cedran <cedran.bastien1@etu.univ-lorraine.fr>
2023-06-15 20:09:37 +0200

    Finishing mane pages view

--- app/Download/dowload.js
+++ app/Download/dowload.js
@@ -3,21 +3,11 @@ import {Card, CardAlert} from "@/app/utils";
 
 
 // Parameters
-const Parameters = ({parameters}) => {
-    let i = 1
-    const html = parameters.map((item) => {
-        return(
-            <div>
-                <p className={"pl-5"}>{i}. {item.name} : {item.explain}</p>
-            </div>
-        )
-        i= i+1
-    })
-
+const Parameters = ({command}) => {
     return (
-        <div>
-            <p className={"underline"}>Parameters : </p>
-            {html}
+        <div className={"flex flex-row gap-1"}>
+            <p className={"underline"}>Run command : </p>
+            <code>{command}</code>
         </div>
     )
 }
@@ -45,18 +35,19 @@ export const Download = () => {
     return (
         <div className={"p-5 mb-8 "}>
             <Card title={"Usage"} titleColor={"text-red-600"} content={"For all State off the architecture, Unzip the compress archive and run the Start.java file"} />
-            <Card title={"Client defendant les calcules"} titleColor={"text-blue-800"} content={<Parameters parameters={parClient} />} HtmlContent={
-                <DownloadHandler filePath='test.zip' buttonName={"Client"}/>
-            }
-            />
-            <Card title={"Service central"} titleColor={"text-blue-800"} content={<Parameters parameters={parCentral} />} HtmlContent={
-                <DownloadHandler filePath={"test.zip"} buttonName={"Server"}/>
-            }
-            />
-            <Card title={"Service calculate"} titleColor={"text-blue-800"} content={<Parameters parameters={parCalcule} />} HtmlContent={
+            <Card title={"Service calculate"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [IP service central]"} />} HtmlContent={
                 <DownloadHandler filePath={"test.zip"} buttonName={"Calculator"}/>
             }
             />
+            <Card title={"Client asking calcule"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [IP service central]"} />} HtmlContent={
+                <DownloadHandler filePath='test.zip' buttonName={"Client"}/>
+            }
+            />
+            <Card title={"Service central"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [port du service]"} />} HtmlContent={
+                <DownloadHandler filePath={"test.zip"} buttonName={"Server"}/>
+            }
+            />
+
         </div>
     )
 }
--- app/Home/home.js
+++ app/Home/home.js
@@ -5,8 +5,12 @@ import Image from "next/image";
 
 
 export const Home = () => {
-    const image =  (<Image width={300} height={0} src={"/logo.png"} alt={"/logo.png"}/>)
+    const image =  (<Image width={1000} height={0} src={"/schema.png"} alt={"/schema.png"}/>)
+    const image2 = (<Image className={"pb-5"} width={1000} height={0} src={"/schema2.png"} alt={"/schema2.png"}/>)
     return (
-            <Card title="Première approche du problème" titleColor={"text-blue-800"} width={"300"} content="c'est bon la" HtmlContent={image}/>
+        <div>
+            <Card title="Première approche du problème" titleColor={"text-blue-800"}  HtmlContent={image}/>
+            <Card title="Fonctionnement definitif" titleColor={"text-blue-800"} HtmlContent={image2}/>
+        </div>
     )
 }
