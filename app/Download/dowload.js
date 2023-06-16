395c9e7ab9c45761da2c62713b46e338f5748ed1
Cedran Bastien <cedran.bastien1@etu.univ-lorraine.fr>
2023-06-16 12:45:07 +0200

    last finition

--- app/Download/dowload.js
+++ app/Download/dowload.js
@@ -36,18 +36,21 @@ export const Download = () => {
     return (
         <div className={"p-5 mb-8 "}>
             <Card title={"Usage"} titleColor={"text-red-600"} content={"For all State off the architecture, Unzip the compress archive and run the Start.java file"} />
-            <Card title={"Service calcul"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [IP service central]"} />} HtmlContent={
-                <DownloadHandler filePath={"service_calcule.zip"} buttonName={"Calculator"}/>
-            }
-            />
-            <Card title={"Client asking calcule"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [IP service central]"} />} HtmlContent={
-                <DownloadHandler filePath='client.zip' buttonName={"Client"}/>
-            }
-            />
-            <Card title={"Service central"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [port du service]"} />} HtmlContent={
+            <Card title={"Service central"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [port de l'annuaire]"} />} HtmlContent={
                 <DownloadHandler filePath={"service_central.zip"} buttonName={"Server"}/>
             }
             />
+            <Card title={"Service compute"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [IP service central] [Port de l'annuaire]"} />} HtmlContent={
+                <DownloadHandler filePath={"service_calcule.zip"} buttonName={"Calculator"}/>
+            }
+            />
+            <Card title={"Client asking compute"} titleColor={"text-blue-800"} content={<Parameters command={"java Start [IP service central]"} />} HtmlContent={
+                <DownloadHandler filePath='client.zip' buttonName={"Client"}/>
+            }
+            />
+
+
+
 
         </div>
     )
--- app/Home/home.js
+++ app/Home/home.js
@@ -8,8 +8,10 @@ import Image from "next/image";
 export const Home = () => {
     const image =  (<Image width={1000} height={0} src={"/schema.png"} alt={"/schema.png"}/>)
     const image2 = (<Image className={"pb-5"} width={1000} height={0} src={"/schema2.png"} alt={"/schema2.png"}/>)
+    const courbe = (<Image className={"pb-5"} width={1000} height={0} src={"/Courbe_data.png"} alt={"/schema2.png"}/>)
     return (
         <div>
+            <Card title="Evolution du temps de calcule en fonction de la taille de l'image" titleColor={"text-blue-800"} HtmlContent={courbe}/>
             <Card title="Première approche du problème" titleColor={"text-blue-800"}  HtmlContent={image}/>
             <Card title="Fonctionnement definitif" titleColor={"text-blue-800"} HtmlContent={image2}/>
         </div>
